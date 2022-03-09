a9abf3cfff6b5234772161dca191bf2a728ef461
hollymsnell <hollymsnell@gmail.com>
2022-03-08 20:40:59 -0700

    added information to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+product = Product.new(name: "chapstick", price: 2, description: "Small compact lip moisturizer with sunscreen")
+product = Product.new(name: "hairbrush", price: 5, description: "brush for wet hair to reduce breakage")
+product = Product.new(name: "water bottle", price: 15, description: "large durable water bottle suitable for rigorous outdoor activity")
+
+puts "all done"
