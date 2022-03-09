9d44d4d93563ade283a8b6f95d175d7f2dfab7d6
hollymsnell <hollymsnell@gmail.com>
2022-03-08 21:01:08 -0700

    added controllers and routes all functional

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+  def all_products_method
+    all_products = Product.all
+    render json: all_products.as_json
+  end
+  def first_product_method
+    first_product = Product.first
+    render json: first_product.as_json
+  end
+  def second_product_method
+    second_product = Product.second
+    render json: second_product.as_json
+  end
+  def third_product_method
+    third_product = Product.third
+    render json: third_product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+get "/all_products", controller: "products", action: "all_products_method"
+get "/first_product", controller: "products", action: "first_product_method"
+get "/second_product" => "products#second_product_method"
+get "/third_product" => "products#third_product_method"
 end
