363bacdc1e112544b01a552da78453c6654ab282
hollymsnell <hollymsnell@gmail.com>
2022-03-24 18:40:59 -0600

    removed quantity and product id from order model

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,4 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :description, :images, :number_available, :supplier
+  attributes :id, :name, :price, :description, :images, :number_available, :supplier, :categories
+
 end
