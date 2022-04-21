edd978b6026402d7f92f1750d4783b76a5b505b6
hollymsnell <hollymsnell@gmail.com>
2022-04-20 19:57:41 -0600

    fixed syntax error on order model

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,11 +4,11 @@ Supplier.create([
 ])
 
 Product.create!([
-  {name: "warm blanket", price: 45, description: "comfortable weighted blanket made for a cozy winter day or night.", number_available: 12},
-  {name: "hairbrush", price: 5, description: "brush for wet hair to reduce breakage", number_available: 45},
-  {name: "chapstick", price: 2, description: "Small compact lip moisturizer with sunscreen", number_available: 80},
-  {name: "cat sweater", price: 20, description: "Fuzzy sweater with cats on it", number_available: 14},
-  {name: "water bottle", price: 15, description: "large durable water bottle suitable for rigorous outdoor activity", number_available: 20}
+  {name: "warm blanket", price: 45, description: "comfortable weighted blanket made for a cozy winter day or night.", number_available: 12, supplier_id: 1},
+  {name: "hairbrush", price: 5, description: "brush for wet hair to reduce breakage", number_available: 45, supplier_id: 2},
+  {name: "chapstick", price: 2, description: "Small compact lip moisturizer with sunscreen", number_available: 80, supplier_id: 2},
+  {name: "cat sweater", price: 20, description: "Fuzzy sweater with cats on it", number_available: 14, supplier_id: 1},
+  {name: "water bottle", price: 15, description: "large durable water bottle suitable for rigorous outdoor activity", number_available: 20, supplier_id: 1}
 ])
 
 
